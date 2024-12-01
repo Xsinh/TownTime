0b015858e0f0f03bbf1c93978a7114917f697b8a
Xsinh <funnybalmosh@gmail.com>
2024-12-01 21:53:55 +0300

    Обработка ошибки

--- app/src/main/java/com/implosion/towntime/data/repository/TimeRepositoryImpl.kt
+++ app/src/main/java/com/implosion/towntime/data/repository/TimeRepositoryImpl.kt
@@ -12,30 +12,33 @@ import java.time.temporal.ChronoField
 class TimeRepositoryImpl(private val apiService: TimeApi) : TimeRepository {
 
     override fun getUpdatedTime(capital: String): Flow<String> = flow {
-        val response = apiService.getData(capital)
-        if (response.isSuccessful) {
-            val responseBody = response.body()
+        try {
+            val response = apiService.getData(capital)
+            if (response.isSuccessful) {
+                val responseBody = response.body()
 
-            val initialLocalDateTime = LocalDateTime.parse(
-                responseBody?.dateTime, DateTimeFormatterBuilder()
-                    .appendPattern("yyyy-MM-dd'T'HH:mm:ss")
-                    .optionalStart()
-                    .appendFraction(ChronoField.NANO_OF_SECOND, 0, 9, true)
-                    .optionalEnd()
+                val initialLocalDateTime = LocalDateTime.parse(
+                    responseBody?.dateTime, DateTimeFormatterBuilder()
+                        .appendPattern("yyyy-MM-dd'T'HH:mm:ss")
+                        .optionalStart()
+                        .appendFraction(ChronoField.NANO_OF_SECOND, 0, 9, true)
+                        .optionalEnd()
+                        .toFormatter()
+                )
+
+                var currentTime = initialLocalDateTime
+
+                val timeFormatter = DateTimeFormatterBuilder()
+                    .appendPattern("HH:mm:ss")
                     .toFormatter()
-            )
 
-            var currentTime = initialLocalDateTime
-
-            val timeFormatter = DateTimeFormatterBuilder()
-                .appendPattern("HH:mm:ss")
-                .toFormatter()
-
-            while (true) {
-                emit(currentTime.format(timeFormatter))
-                delay(1000)
-                currentTime = currentTime.plusSeconds(1)
+                while (true) {
+                    emit(currentTime.format(timeFormatter))
+                    delay(1000)
+                    currentTime = currentTime.plusSeconds(1)
+                }
             }
+        } catch (ignored: Exception) {
         }
     }
 }
